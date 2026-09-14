85c2ff154d4b97569d410d443f5ebee3bb475960
AlSleepless <bessonov853@gmail.com>
2026-09-14 22:48:09 +0500

    person header

---
+++ include/Person.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <string>
+
+namespace stayout {
+
+struct Person {
+  std::string firstName;
+  std::string lastName;
+  std::string phoneNumber;
+};
+
+} // namespace stayout
