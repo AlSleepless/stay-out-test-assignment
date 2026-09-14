4350ac56dce7f71e7d1f9797882de3563ae74664
AlSleepless <bessonov853@gmail.com>
2026-09-14 23:08:19 +0500

    sorter header

---
+++ include/Sorter.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Person.hpp"
+
+#include <cstdint>
+#include <vector>
+
+namespace stayout {
+
+enum class SortOption : std::uint8_t {
+  ByFirstName = 1,   //!< сортировка по имени
+  ByLastName = 2,    //!< сортировка по фамилии
+  ByPhoneNumber = 3, //!< сортировка по телефону
+};
+
+/**
+ * @brief Сортирует массив people по указанному option принципу
+ *
+ * @param people массив для сортировки
+ * @param option вариант сортировки
+ */
+void sortPeople(std::vector<Person> &people, SortOption option);
+
+} // namespace stayout
