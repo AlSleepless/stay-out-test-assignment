4dfb63a7ce5761dd6bcb4d87f4420f6f41e650de
AlSleepless <bessonov853@gmail.com>
2026-09-14 23:11:30 +0500

    printer header

---
+++ include/Printer.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "Person.hpp"
+#include "Sorter.hpp"
+
+#include <ostream>
+#include <vector>
+
+namespace stayout {
+
+/**
+ * @brief Вывести массив персон
+ *
+ * @param os поток вывода
+ * @param people массив персон для вывода
+ */
+void print(std::ostream &os, std::vector<Person> people);
+
+} // namespace stayout
