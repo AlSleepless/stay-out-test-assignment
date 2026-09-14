951a1c4ebb0fa0a7527ae156f0f4094929758e89
AlSleepless <bessonov853@gmail.com>
2026-09-14 23:37:06 +0500

    utest parse line

--- tests/test_parser.cpp
+++ tests/test_parser.cpp
@@ -13,3 +13,41 @@
 #include <gtest/gtest.h>
 
 using namespace stayout;
+
+// parse line
+
+TEST(ParserTest, ValidLine) {
+  Person p;
+  ASSERT_TRUE(Parser::parseLine("Бессонов Александр: 0123456789", p));
+  EXPECT_EQ(p.lastName, "Бессонов");
+  EXPECT_EQ(p.firstName, "Александр");
+  EXPECT_EQ(p.phoneNumber, "0123456789");
+}
+
+TEST(ParserTest, InvalidLineEmpty) {
+  Person p;
+  EXPECT_FALSE(Parser::parseLine("", p));
+}
+
+TEST(ParserTest, InvalidLineNoSpaces) {
+  Person p;
+  EXPECT_FALSE(Parser::parseLine("БессоновАлександр:0123456789", p));
+}
+
+TEST(ParserTest, InvalidLineNoColon) {
+  Person p;
+  EXPECT_FALSE(Parser::parseLine("Бессонов Александр 0123456789", p));
+}
+
+TEST(ParserTest, InvalidLineNoPhone) {
+  Person p;
+  EXPECT_FALSE(Parser::parseLine("Бессонов Александр: ", p));
+}
+
+TEST(ParserTest, InvalidLineThirdName) {
+  Person p;
+  EXPECT_FALSE(
+      Parser::parseLine("Бессонов Александр Вячеславович: 0123456789", p));
+}
+
+// TODO load from file
