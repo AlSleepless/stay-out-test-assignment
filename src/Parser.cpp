0764a48394dc4b01bebaee4a042eff5e281cc348
AlSleepless <bessonov853@gmail.com>
2026-09-15 19:29:45 +0500

    source parser green

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -8,3 +8,64 @@
  * @copyright Copyright (c) 2026
  *
  */
+
+#include "Parser.hpp"
+#include <cerrno>
+#include <cstring>
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+
+namespace stayout {
+
+bool Parser::parseLine(std::string_view line, Person &out) {
+  if (line.empty())
+    return false; // строка пуста
+
+  const auto colonPos = line.find(": ");
+  if (colonPos == std::string_view::npos)
+    return false; // нет двоеточия
+
+  auto fullName = line.substr(0, colonPos);
+  auto phoneNumber = line.substr(colonPos + 2);
+  if (phoneNumber.empty())
+    return false; // нет номера телефона
+
+  const auto spacePos = fullName.find(' ');
+  if (spacePos == std::string_view::npos)
+    return false; // нет пробела
+  if (fullName.find(' ', spacePos + 1) != std::string_view::npos)
+    return false; // более одного пробела
+
+  std::string_view lastName = fullName.substr(0, spacePos);
+  std::string_view firstName = fullName.substr(spacePos + 1);
+  if (lastName.empty() or firstName.empty())
+    return false; // нет имени или фамилии
+
+  out.lastName = std::string(lastName);
+  out.firstName = std::string(firstName);
+  out.phoneNumber = std::string(phoneNumber);
+  return true;
+}
+
+std::vector<Person> Parser::loadFromStream(std::istream &iStream) const {
+  std::vector<Person> people;
+  std::string line;
+  while (std::getline(iStream, line)) {
+    Person person;
+    if (parseLine(line, person)) {
+      people.push_back(std::move(person));
+    }
+  }
+  return people;
+}
+
+std::vector<Person> Parser::loadFromFile(std::string_view path) const {
+  std::ifstream fd{std::string(path)};
+  if (!fd.is_open())
+    throw std::runtime_error(std::string(path) + " : code " +
+                             std::strerror(errno));
+  return loadFromStream(fd);
+}
+
+} // namespace stayout
