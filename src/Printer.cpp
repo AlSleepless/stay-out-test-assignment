afaba3d4e49e4f0f4a742cae9c65271086031feb
AlSleepless <bessonov853@gmail.com>
2026-09-15 21:19:08 +0500

    source printer green

--- src/Printer.cpp
+++ src/Printer.cpp
@@ -8,3 +8,34 @@
  * @copyright Copyright (c) 2026
  *
  */
+
+#include "Printer.hpp"
+#include "Sorter.hpp"
+
+#include <ostream>
+
+namespace stayout {
+
+void printSorted(std::ostream &os, std::vector<Person> people,
+                 SortOption option) {
+  sortPeople(people, option);
+
+  for (const auto &person : people) {
+    switch (option) {
+    case SortOption::ByFirstName:
+      os << person.firstName << ' ' << person.lastName << ": "
+         << person.phoneNumber << '\n';
+      break;
+    case SortOption::ByLastName:
+      os << person.lastName << ' ' << person.firstName << ": "
+         << person.phoneNumber << '\n';
+      break;
+    case SortOption::ByPhoneNumber:
+      os << person.phoneNumber << ": " << person.lastName << ' '
+         << person.firstName << '\n';
+      break;
+    }
+  }
+}
+
+} // namespace stayout
